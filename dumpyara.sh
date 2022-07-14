ebb5516b2dd41d90e54f4a47e77ecd9cd6b8e38e
P Salik <78614601+P-Salik@users.noreply.github.com>
2022-07-14 16:19:41 +0530

    Don't use sudo
    * cirrus-ci doesn't allow the use of sudo

--- dumpyara.sh
+++ dumpyara.sh
@@ -117,12 +117,12 @@ for p in $PARTITIONS; do
                     rm -fv "$p".img > /dev/null 2>&1
                 else
                     echo "Couldn't extract $p partition by fsck.erofs. Using mount loop"
-                    sudo mount -o loop -t auto "$p".img "$p"
+                    mount -o loop -t auto "$p".img "$p"
                     mkdir "${p}_"
-                    sudo cp -rf "${p}/"* "${p}_"
-                    sudo umount "${p}"
-                    sudo cp -rf "${p}_/"* "${p}"
-                    sudo rm -rf "${p}_"
+                    cp -rf "${p}/"* "${p}_"
+                    umount "${p}"
+                    cp -rf "${p}_/"* "${p}"
+                    rm -rf "${p}_"
                     if [ $? -eq 0 ]; then
                         rm -fv "$p".img > /dev/null 2>&1
                     else
@@ -137,8 +137,8 @@ for p in $PARTITIONS; do
 done
 
 # Fix permissions
-sudo chown "$(whoami)" "$PROJECT_DIR"/working/"${UNZIP_DIR}"/./* -fR
-sudo chmod -fR u+rwX "$PROJECT_DIR"/working/"${UNZIP_DIR}"/./*
+chown "$(whoami)" "$PROJECT_DIR"/working/"${UNZIP_DIR}"/./* -fR
+chmod -fR u+rwX "$PROJECT_DIR"/working/"${UNZIP_DIR}"/./*
 
 # board-info.txt
 find "$PROJECT_DIR"/working/"${UNZIP_DIR}"/modem -type f -exec strings {} \; | grep "QC_IMAGE_VERSION_STRING=MPSS." | sed "s|QC_IMAGE_VERSION_STRING=MPSS.||g" | cut -c 4- | sed -e 's/^/require version-baseband=/' >> "$PROJECT_DIR"/working/"${UNZIP_DIR}"/board-info.txt
